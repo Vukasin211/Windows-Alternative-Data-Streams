6292314ecd3acde0aaf7b02d0c6c278c85b01b03
Vukasin211 <vukasinp@elfak.rs>
2025-09-03 13:56:04 +0200

    Basic File Explorer
    Pocetak

---
+++ DF Projekat/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DF_Projekat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(379, 450);
+            this.treeView.TabIndex = 0;
+            // 
+            // listView
+            // 
+            this.listView.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(430, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(370, 450);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ DF Projekat/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DF_Projekat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            treeView.BeforeExpand += TreeView_BeforeExpand;
+            treeView.AfterSelect += TreeView_AfterSelect;
+            listView.MouseDoubleClick += ListView_MouseDoubleClick;
+            listView.View = View.List;
+        }
+        //Nakon sto je dva puta kliknut file u listView
+        private void ListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(listView.SelectedItems.Count>0)
+            {
+                string filepath = listView.SelectedItems[0].Tag.ToString();
+                try
+                {
+                    System.Diagnostics.Process.Start(filepath);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("There is an Issue with " + ex.Message.ToString());
+                }
+            }
+        }
+
+        //Nakon sto je odabran folder u treeView
+        private void TreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = e.Node.Tag.ToString();
+            listView.Items.Clear();
+            try
+            {
+                string[] files = Directory.GetFiles(path);
+                foreach(string file in files)
+                {
+                    ListViewItem item = new ListViewItem(Path.GetFileName(file));
+                    item.Tag = file;
+                    item.SubItems.Add(new FileInfo(file).Length.ToString() + " bytes");
+                    listView.Items.Add(item);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("There is an Issue with " + ex.Message.ToString());
+            }
+        }
+
+        private void TreeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            node.Nodes.Clear();
+            try
+            {
+                string[] directories = Directory.GetDirectories(node.Tag.ToString());
+                foreach (string directory in directories)
+                {
+                    TreeNode subNode = new TreeNode(Path.GetFileName(directory));
+                    subNode.Tag = directory;
+                    subNode.Nodes.Add("Loading...");
+                    node.Nodes.Add(subNode);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There is an Issue with " + ex.Message.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(var drive in Directory.GetLogicalDrives())
+            {
+                TreeNode node = new TreeNode(drive);
+                node.Tag = drive;
+                node.Nodes.Add("Loading...");
+                treeView.Nodes.Add(node);
+            }
+        }
+    }
+}
