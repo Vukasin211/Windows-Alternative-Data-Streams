35905e739bc8b33d863575a7c78e56b97fd08a78
Vukasin211 <vukasinp@elfak.rs>
2025-09-04 13:10:33 +0200

    ADS dodaci
    Dodate kontrole za citanje i upis ADS fajlova, kao i funkcionalnost kreiranja ADS root fajla na koji ce se kaciti ostali binarni fajlovi

--- DF Projekat/Form1.Designer.cs
+++ DF Projekat/Form1.Designer.cs
@@ -30,36 +30,151 @@
         {
             this.treeView = new System.Windows.Forms.TreeView();
             this.listView = new System.Windows.Forms.ListView();
+            this.StreamKeyValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StoreRadio = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.OpenRadio = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ReadButton = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView
             // 
-            this.treeView.Dock = System.Windows.Forms.DockStyle.Left;
-            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.treeView.Location = new System.Drawing.Point(11, 73);
             this.treeView.Name = "treeView";
-            this.treeView.Size = new System.Drawing.Size(379, 450);
+            this.treeView.Size = new System.Drawing.Size(268, 419);
             this.treeView.TabIndex = 0;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect_1);
             // 
             // listView
             // 
-            this.listView.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(430, 0);
+            this.listView.Location = new System.Drawing.Point(557, 73);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(370, 450);
+            this.listView.Size = new System.Drawing.Size(268, 419);
             this.listView.TabIndex = 1;
             this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // StreamKeyValue
+            // 
+            this.StreamKeyValue.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StreamKeyValue.Location = new System.Drawing.Point(6, 44);
+            this.StreamKeyValue.Name = "StreamKeyValue";
+            this.StreamKeyValue.Size = new System.Drawing.Size(253, 32);
+            this.StreamKeyValue.TabIndex = 2;
+            this.StreamKeyValue.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Stream Key";
+            // 
+            // StoreRadio
+            // 
+            this.StoreRadio.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StoreRadio.Location = new System.Drawing.Point(6, 9);
+            this.StoreRadio.Name = "StoreRadio";
+            this.StoreRadio.Size = new System.Drawing.Size(130, 39);
+            this.StoreRadio.TabIndex = 4;
+            this.StoreRadio.TabStop = true;
+            this.StoreRadio.Text = "Store";
+            this.StoreRadio.UseVisualStyleBackColor = true;
+            this.StoreRadio.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.OpenRadio);
+            this.groupBox1.Controls.Add(this.StoreRadio);
+            this.groupBox1.Location = new System.Drawing.Point(11, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 54);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // OpenRadio
+            // 
+            this.OpenRadio.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenRadio.Location = new System.Drawing.Point(142, 9);
+            this.OpenRadio.Name = "OpenRadio";
+            this.OpenRadio.Size = new System.Drawing.Size(126, 39);
+            this.OpenRadio.TabIndex = 5;
+            this.OpenRadio.TabStop = true;
+            this.OpenRadio.Text = "Open";
+            this.OpenRadio.UseVisualStyleBackColor = true;
+            this.OpenRadio.CheckedChanged += new System.EventHandler(this.ReadRadio_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.StreamKeyValue);
+            this.groupBox2.Location = new System.Drawing.Point(286, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(265, 90);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(293, 169);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(253, 53);
+            this.SaveButton.TabIndex = 7;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // ReadButton
+            // 
+            this.ReadButton.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReadButton.Location = new System.Drawing.Point(293, 227);
+            this.ReadButton.Name = "ReadButton";
+            this.ReadButton.Size = new System.Drawing.Size(253, 53);
+            this.ReadButton.TabIndex = 8;
+            this.ReadButton.Text = "Read";
+            this.ReadButton.UseVisualStyleBackColor = true;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.StatusLabel.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Location = new System.Drawing.Point(293, 292);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(253, 55);
+            this.StatusLabel.TabIndex = 9;
+            this.StatusLabel.Text = "Ready";
+            this.StatusLabel.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(837, 503);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.ReadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listView);
             this.Controls.Add(this.treeView);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +183,15 @@
 
         private System.Windows.Forms.TreeView treeView;
         private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.TextBox StreamKeyValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton StoreRadio;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton OpenRadio;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button ReadButton;
+        private System.Windows.Forms.Label StatusLabel;
     }
 }
 
--- DF Projekat/Form1.cs
+++ DF Projekat/Form1.cs
@@ -20,6 +20,7 @@ namespace DF_Projekat
             treeView.AfterSelect += TreeView_AfterSelect;
             listView.MouseDoubleClick += ListView_MouseDoubleClick;
             listView.View = View.List;
+            StoreRadio.Checked = true;
         }
         //Nakon sto je dva puta kliknut file u listView
         private void ListView_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -43,20 +44,23 @@ namespace DF_Projekat
         {
             string path = e.Node.Tag.ToString();
             listView.Items.Clear();
-            try
+            if(StoreRadio.Checked)
             {
-                string[] files = Directory.GetFiles(path);
-                foreach(string file in files)
+                try
                 {
-                    ListViewItem item = new ListViewItem(Path.GetFileName(file));
-                    item.Tag = file;
-                    item.SubItems.Add(new FileInfo(file).Length.ToString() + " bytes");
-                    listView.Items.Add(item);
+                    string[] files = Directory.GetFiles(path);
+                    foreach (string file in files)
+                    {
+                        ListViewItem item = new ListViewItem(Path.GetFileName(file));
+                        item.Tag = file;
+                        item.SubItems.Add(new FileInfo(file).Length.ToString() + " bytes");
+                        listView.Items.Add(item);
+                    }
                 }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("There is an Issue with " + ex.Message.ToString());
+                catch (Exception ex)
+                {
+                    MessageBox.Show("There is an Issue with " + ex.Message.ToString());
+                }
             }
         }
 
@@ -89,6 +93,73 @@ namespace DF_Projekat
                 node.Tag = drive;
                 node.Nodes.Add("Loading...");
                 treeView.Nodes.Add(node);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReadRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView_AfterSelect_1(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if(StoreRadio.Checked)
+            {
+                // Check if something is selected in TreeView
+                if (treeView.SelectedNode == null)
+                {
+                    StatusLabel.Text = "No folder selected!";
+                    return;
+                }
+
+                // Get selected folder path
+                string selectedPath = treeView.SelectedNode.Tag.ToString();
+
+                // Define root.txt path
+                string rootFilePath = Path.Combine(selectedPath, "root.txt");
+
+                try
+                {
+                    if (File.Exists(rootFilePath))
+                    {
+                        // File already exists
+                        StatusLabel.Text = "Exists";
+                    }
+                    else
+                    {
+                        // Create the file
+                        using (FileStream fs = File.Create(rootFilePath))
+                        {
+                            // Just creating an empty file is enough
+                        }
+
+                        StatusLabel.Text = "Created Root";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
             }
         }
     }
